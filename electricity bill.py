2688d274e035536ce415a3845d7f4337f4892297
MOHAMMAD HAFEEZ <alambhaiisback@gmail.com>
2026-09-05 23:14:31 +0530

    Add electricity bill calculation functionality

---
+++ electricity bill.py
@@ -0,0 +1,15 @@
+units = float(input("Enter electricity units: "))
+
+if units <= 100:
+    bill = units * 5
+
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+
+elif units <= 300:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+
+else:
+    bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 12)
+
+print("Electricity Bill:", bill)
