d3addf421238e8e26f8459838dfb8f3011fc2360
MOHAMMAD HAFEEZ <alambhaiisback@gmail.com>
2026-09-08 23:56:09 +0530

    Add age calculator, percentage calculator, and coin toss functionality

---
+++ persentage calculater.py
@@ -0,0 +1,6 @@
+total = float(input("Enter total marks: "))
+obtained = float(input("Enter obtained marks: "))
+
+percentage = (obtained / total) * 100
+
+print("Percentage:", percentage, "%")
---
+++ simple coin tush.py
@@ -0,0 +1,5 @@
+import random
+
+coin = random.choice(['Heads', 'Tails'])
+
+print("Coin Toss Result:", coin)
